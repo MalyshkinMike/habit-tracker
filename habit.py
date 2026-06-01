a9bcfd9ca8eb96d05f2a4e2e0184aaf80690bc86
Mike <mikhail.malishkin@gmail.com>
2026-06-01 18:01:42 +0400

    POC Habic class added

---
+++ habit.py
@@ -0,0 +1,29 @@
+
+class Habit:
+
+    def __init__(self, name, base_xp, habit_days, xp_growth_coef):
+        self.name = name
+        self.habit_days = habit_days
+        self.base_xp = base_xp 
+        self.xp_growth_coef = xp_growth_coef
+        self.__today_submition = False
+        self.__days_streak = 0
+
+    def submit_habit(self, user):
+        if (self.__today_submition):
+            raise Exception("Already submitted this habit")
+        user.gain_xp(self.calculate_xp())
+        self.__today_submition = True
+        self.__days_streak += 1
+
+    def restart_day(self):
+        if not self.__today_submition:
+            self__days_streak = 0
+        self.__today_submition = False
+
+    def calculate_xp(self) -> int:
+        xp = self.base_xp + self.__days_streak * self.xp_growth_coef
+
+
+
+    
